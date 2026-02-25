6a7822b83856185d5a20d7cda47373c74513dcd1
P.Shasank Pavan <pennerushasankpavan@gmail.com>
2026-02-25 22:03:34 +0530

    webhook auto trigger test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     build_id = os.getenv('BUILD_ID', 'Local')
-    return f"<h1>Hello from Jenkins CI/CD!</h1><p>Deployed via Jenkins Webhook. Build ID: {build_id}</p>"
+    return f"<h1>Hello from Jenkins CI/CD!hello from Jenkins</h1><p>Deployed via Jenkins Webhook. Build ID: {build_id}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
