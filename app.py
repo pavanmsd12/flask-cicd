be70cab1ca0dc8e37ecccaf7ffdfc3fe2f2245d4
P.Shasank Pavan <pennerushasankpavan@gmail.com>
2026-02-25 20:01:41 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    build_id = os.getenv('BUILD_ID', 'Local')
+    return f"<h1>Hello from Flask!</h1><p>Deployed via Jenkins Webhook. Build ID: {build_id}</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
